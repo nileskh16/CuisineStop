b61b9a8e170ab2c17bd83f84140ec8b049a421c4
nileskh16 <nileshfbk123@gmail.com>
2016-12-28 17:00:40 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,69 +1,75 @@
-'use-strict';
+(function(){
+	'use-strict';
 
-angular.module('skapp', ['ui.router'])
-  .config(RouteConfig)
-  .service('list', list)
-  .controller('homeCon', homeCon)
-  .controller('catCon', catCon)
-  .controller('itemCon', itemCon)
+var myapp = angular.module('SK', [])
+	.controller('skcon', skcon)
+	.service('Mservice', Mservice);
+	
+skcon.$inject = ['Mservice'];
+function skcon(Mservice){
+	var sk = this;
+	sk.food_name = '';
+	sk.got = [];
+	sk.didyou = true;
+	sk.fetch_data = function(food_name){
+	sk.prom = Mservice.getItems(food_name);
+	sk.prom.then(function(data){
+		if(data.length <= 0)
+		{
+			sk.didyou = false;
+		}
+		else
+		{
+			sk.got = data;
+			sk.didyou = true;
+			console.log(data);
+		}
+	}).catch(function(error){
+		console.log(error)
+		sk.didyou = false;
+	});
+	};
+	
+	sk.rmitem = function(index){
+		sk.got.splice(index,1);
+	};
+}
 
-RouteConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
-function RouteConfig($stateProvider, $urlRouterProvider){
-  //$urlRouterProvider.otherwise('/index');
-  $stateProvider
-  .state('categories', {
-    url: '/categories',
-    templateUrl: 'categories.html',
-    controller: 'catCon as con',
-    resolve:{
-      items: ['list', function(list){
-        return list.getItems();
-      }]
-    }
-  })
-  .state('index', {
-    url: '/index',
-    templateUrl: 'index.html',
-  })
-  .state('categories.item',{
-    url: '/item/{id}',
-    templateUrl: 'item.html',
-    controller: 'itemCon as con'
-  });
-};
+Mservice.$inject = ['$http', '$q', '$timeout'];
 
-list.$inject = ['$q', '$timeout'];
-function list($q, $timeout){
-  var service = this;
-  var items = [];
-  items.push({name:'American', en: ['Hotdogs', 'Pancakes', 'Burgers', 'Donut', 'Pies']});
-  items.push({name:'Italian', en: ['Pizza', 'Pasta', 'Maggie']});
-  items.push({name:'Indian', en: ['Rotee', 'Panipuri', 'Rabadi']});
-  items.push({name:'Mexican', en: ['Chilie', 'Nacho']});
-  items.push({name:'Chinese', en: ['Rice', 'Noodles', 'Chopsie']});
-
-  service.getItems = function(){
-    var prom = $q.defer();
-    $timeout(function () {
-      prom.resolve(items);
-    }, 2000);
-    return prom.promise;
-  }
-};
-
-homeCon.$inject = ['$scope'];
-function homeCon($scope){
-  var con = this;
-};
-
-catCon.$inject = ['$scope', 'items'];
-function catCon($scope, items){
-  var con = this;
-  con.items = items;
-};
-
-itemCon.$inject = ['$scope', 'items', '$stateParams'];
-function itemCon($scope, items, $stateParams){
-  var con = this;
-  con.item = items[$stateParams.id];
-};
+function Mservice($http, $q, $timeout){
+	var service = this;
+	var got = false;
+	service.getItems = function(desc){
+		var prom = $q.defer();
+		console.log(desc)
+		if(desc != '')
+		{
+			$http({
+				method: 'GET',
+				url: 'https://davids-restaurant.herokuapp.com/menu_items.json'
+			}).then(function(resp){
+				service.result = resp.data.menu_items;
+				found = [];
+				//console.log(service.result[0]);
+				for(i=0; i<service.result.length; i++)
+				{
+					var got = true;
+					var item = service.result[i];
+					if(item.description.indexOf(desc) != -1){
+						found.push(item);
+					}
+				}
+				prom.resolve(found);
+			},
+			function(error){
+				console.log(error.message)
+				prom.reject('Something went wrong');
+			});
+		}
+		else
+			prom.reject('Triffling data was consigned.');
+	return prom.promise;
+	};
+}
+})();
